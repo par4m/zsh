9f0f098743981e9355a8d9eee1b68a2c11a542ea
par4m <param2k3@gmail.com>
2022-08-01 12:11:37 +0530

    fzf nord

--- fzf-themes/nord.sh
+++ fzf-themes/nord.sh
@@ -1,8 +1,8 @@
 export FZF_DEFAULT_OPTS='
-  --color fg:#5d6466,bg:#252933
-  --color bg+:#ef7d7d,fg+:#2c2f30
-  --color hl:#dadada,hl+:#26292a,gutter:#252933
+  --color fg:#bebebe,bg:#333A47
+  --color bg+:#B48EAD,fg+:#2c2f30
+  --color hl:#dadada,hl+:#26292a,gutter:#333A47
   --color pointer:#373d49,info:#606672
   --border
-  --color border:#252933
+  --color border:#333A47
   --height 13'
